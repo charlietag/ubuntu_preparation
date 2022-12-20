1db06939baab75a3ecbbc41428af689831012446
Charlie Hsieh <charlieofficial@gmail.com>
2022-12-21 03:05:48 +0800

    dnf -> apt

--- functions/F_01_ENV_03_basic_00_pkgs_remove.sh
+++ functions/F_01_ENV_03_basic_00_pkgs_remove.sh
@@ -7,11 +7,13 @@
 local pkg_exists
 for remove_pkg_name in ${remove_pkg_names[@]}; do
   pkg_exists=""
-  pkg_exists="$(rpm -qa | grep "${remove_pkg_name}")"
+  pkg_exists="$(dpkg -l ${remove_pkg_name} 2>/dev/null | grep -E "^ii")"
 
   if [[ -n "${pkg_exists}" ]]; then
     echo "Removing pkg :  ${remove_pkg_name} ..."
-    dnf remove -y ${remove_pkg_name}
+    # apt purge -y ${remove_pkg_name}
+    # apt autoremove -y ${remove_pkg_name}
+    apt remove --purge --autoremove -y ${remove_pkg_name}
   fi
 done
 
