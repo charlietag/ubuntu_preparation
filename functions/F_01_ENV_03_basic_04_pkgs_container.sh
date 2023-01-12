57640c3d38ae52184cd985607a0cf8fcdf84df35
Charlie Hsieh <charlieofficial@gmail.com>
2023-01-13 02:11:59 +0800

    podman for ubuntu22

--- functions/F_01_ENV_03_basic_04_pkgs_container.sh
+++ functions/F_01_ENV_03_basic_04_pkgs_container.sh
@@ -2,17 +2,14 @@ echo "==============================="
 echo "  Installing Container Management packages..."
 echo "==============================="
 
-dnf groupinstall -y "Container Management"
-
 local pkgs_list=""
 
 pkgs_list="${pkgs_list} podman buildah skopeo"
 
-pkgs_list="${pkgs_list} podman-compose"
-
-pkgs_list="${pkgs_list} runc conmon criu udica container-selinux"
-
 #-----------------------------------------------------------------------------------------
 #Package Start to Install
 #-----------------------------------------------------------------------------------------
-dnf install -y ${pkgs_list}
+apt install -y ${pkgs_list}
+
+apt install -y python3-pip
+pip3 install podman-compose
