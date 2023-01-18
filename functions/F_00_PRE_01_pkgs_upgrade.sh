6ab73e73ce70d1a1a1e8727c75f3309eb87cf6f1
Charlie Hsieh <charlieofficial@gmail.com>
2023-01-18 23:58:40 +0800

    apt install(upgrade) multiple packages in one time

--- functions/F_00_PRE_01_pkgs_upgrade.sh
+++ functions/F_00_PRE_01_pkgs_upgrade.sh
@@ -1,4 +1,4 @@
 apt update
 apt upgrade -y
 apt update
-apt list --upgradable 2>/dev/null | grep upgradable | awk -F'/' '{print $1}' | xargs -I {} apt install -y {}
+apt list --upgradable 2>/dev/null | grep upgradable | awk -F'/' '{print $1}' | xargs apt install -y
