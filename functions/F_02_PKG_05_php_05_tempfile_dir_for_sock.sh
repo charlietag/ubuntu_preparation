3000b8061f098ac15cc226a147242232530e8140
Charlie Hsieh <charlieofficial@gmail.com>
2023-01-22 02:58:24 +0800

    Use TCP port listening instead of socket file listening

--- functions/F_02_PKG_05_php_05_tempfile_dir_for_sock.sh
+++ functions/F_02_PKG_05_php_05_tempfile_dir_for_sock.sh
@@ -1,9 +1,11 @@
 # =====================
 # Enable databag
 # =====================
-# DATABAG_CFG:enable
+# DATABAG_CFG:disable
 
 #--------------------------
 #  Setup tmpfiles.d configs
 #--------------------------
-task_copy_using_render
+# task_copy_using_render
+echo "Skip this function:"
+echo "Use TCP port listening instead of socket file listening"
