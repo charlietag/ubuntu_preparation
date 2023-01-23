2856332e56dff7085f97485ad6803c40c023cd9c
Charlie Hsieh <charlieofficial@gmail.com>
2023-01-24 01:45:48 +0800

    default disable podman service

--- functions/F_01_ENV_03_basic_04_pkgs_container.sh
+++ functions/F_01_ENV_03_basic_04_pkgs_container.sh
@@ -13,3 +13,7 @@ apt install -y ${pkgs_list}
 
 apt install -y python3-pip
 pip3 install podman-compose
+
+
+# Disable related services
+systemctl list-unit-files|grep podman | awk '{print $1}' | xargs echo  | xargs -I{} bash -c "systemctl stop {}; systemctl disable {}"
--- functions/F_91_MA_toggle_service.sh
+++ functions/F_91_MA_toggle_service.sh
@@ -12,21 +12,33 @@ local service_name=""
 echo "================================"
 echo "    Toggling Services"
 echo "================================"
+echo "${toggle}: --->"
+echo "${toggle_services[@]}"
+echo "${toggle}: ---> done"
 
-for toggle_service in ${toggle_services[@]}
-do
-  service_name="${toggle_service}.service"
-  echo ""
-  echo "------------------------------------------"
-  echo "systemctl ${toggle} \"${service_name}\""
-  echo "------------------------------------------"
+if [ "${toggle}" == "enable" ]
+then
+  systemctl enable ${toggle_services[@]}
+  systemctl start ${toggle_services[@]}
+else
+  systemctl disable ${toggle_services[@]}
+  systemctl stop ${toggle_services[@]}
+fi
 
-  if [ "${toggle}" == "enable" ]
-  then
-    systemctl enable ${service_name}
-    systemctl start ${service_name}
-  else
-    systemctl disable ${service_name}
-    systemctl stop ${service_name}
-  fi
-done
+# for toggle_service in ${toggle_services[@]}
+# do
+#   service_name="${toggle_service}.service"
+#   echo ""
+#   echo "------------------------------------------"
+#   echo "systemctl ${toggle} \"${service_name}\""
+#   echo "------------------------------------------"
+#
+#   if [ "${toggle}" == "enable" ]
+#   then
+#     systemctl enable ${service_name}
+#     systemctl start ${service_name}
+#   else
+#     systemctl disable ${service_name}
+#     systemctl stop ${service_name}
+#   fi
+# done
