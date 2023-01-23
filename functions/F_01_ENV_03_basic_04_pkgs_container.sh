1b04c411d7812b50773a24a55b0cd0963d52f6a9
Charlie Hsieh <charlieofficial@gmail.com>
2023-01-24 02:26:18 +0800

    add ref links

--- functions/F_01_ENV_03_basic_04_pkgs_container.sh
+++ functions/F_01_ENV_03_basic_04_pkgs_container.sh
@@ -11,7 +11,9 @@ pkgs_list="${pkgs_list} podman buildah skopeo"
 #-----------------------------------------------------------------------------------------
 apt install -y ${pkgs_list}
 
+# Ref. https://packaging.python.org/en/latest/guides/installing-using-linux-tools/#debian-ubuntu
 apt install -y python3-pip python3-venv
+
 # WARNING: Running pip as the 'root' user can result in broken permissions and conflicting behaviour with the system package manager. It is recommended to use a virtual environment instead: https://pip.pypa.io/warnings/venv
 # pip3 install podman-compose
 
--- functions/F_02_PKG_07_python_00_user_pythonuser.sh
+++ functions/F_02_PKG_07_python_00_user_pythonuser.sh
@@ -10,4 +10,5 @@ helper_env_user_base
 task_copy_using_cat_user_home
 
 # --- already installed while installing pkgs_container ---
+# Ref. https://packaging.python.org/en/latest/guides/installing-using-linux-tools/#debian-ubuntu
 # apt install -y python3-pip python3-venv
