d7fa8fede0d76d566813f0772c63a58e9a93c096
Charlie Hsieh <charlieofficial@gmail.com>
2023-01-24 11:30:08 +0800

    make sure podman using cgroupfs instead of systemd

--- functions/F_01_ENV_03_basic_04_pkgs_container.sh
+++ functions/F_01_ENV_03_basic_04_pkgs_container.sh
@@ -18,5 +18,11 @@ apt install -y python3-pip python3-venv
 # pip3 install podman-compose
 
 
+#-----------------------------------------------------------------------------------------
+# Podman 3.x
+#-----------------------------------------------------------------------------------------
 # Disable related services
 systemctl list-unit-files|grep podman | awk '{print $1}' | xargs echo  | xargs -I{} bash -c "systemctl stop {}; systemctl disable {}"
+if [[ -f /etc/containers/libpod.conf ]]; then
+  sed -re '/cgroup_manager/ s/systemd/cgroupfs/g' -i /etc/containers/libpod.conf
+fi
