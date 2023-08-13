a6cce42739b5ac598e347b685fd541fff7220029
Charlie Hsieh <charlieofficial@gmail.com>
2023-08-14 00:17:56 +0800

    typo

--- functions/F_02_PKG_07_python_00_user_pythonuser.sh
+++ functions/F_02_PKG_07_python_00_user_pythonuser.sh
@@ -11,4 +11,4 @@ task_copy_using_cat_user_home
 
 # --- already installed while installing pkgs_container ---
 # Ref. https://packaging.python.org/en/latest/guides/installing-using-linux-tools/#debian-ubuntu
-# apt install -y python3-pip python3-venv
+# python3-pip python3-venv
