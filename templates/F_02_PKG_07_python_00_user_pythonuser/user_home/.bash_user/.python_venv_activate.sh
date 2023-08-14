fbffdf77787c6fc37ad6a621da8118d134e2f781
Charlie Hsieh <charlieofficial@gmail.com>
2023-08-15 07:41:48 +0800

    python venv in tmux automatically activate

--- templates/F_02_PKG_07_python_00_user_pythonuser/user_home/.bash_user/.python_venv_activate.sh
+++ templates/F_02_PKG_07_python_00_user_pythonuser/user_home/.bash_user/.python_venv_activate.sh
@@ -1,4 +1,19 @@
 # this script is designed for "poetry" in TMUX
-if [[ -n "${VIRTUAL_ENV}" ]]; then
-  . $VIRTUAL_ENV/bin/activate
+
+
+if [[ -f pyproject.toml ]]; then
+  POETRY_COMMAND="$(command -v poetry)"
+  if [[ -n "${POETRY_COMMAND}" ]]; then
+    MY_PYTHON_VIRTUAL_ENV="$(poetry env info | grep poetry | grep virtualenvs | grep Path | awk '{print $2}')"
+  fi
+elif [[ -f bin/activate ]]; then
+  VIRTUAL_ENV_CHECK="$(find ./ -name virtualenv.py)"
+  if [[ -n "${VIRTUAL_ENV_CHECK}" ]]; then
+    MY_PYTHON_VIRTUAL_ENV="$(pwd)"
+  fi
+fi
+
+
+if [[ -n "${MY_PYTHON_VIRTUAL_ENV}" ]]; then
+  . $MY_PYTHON_VIRTUAL_ENV/bin/activate
 fi
